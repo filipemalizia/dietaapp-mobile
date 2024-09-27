51d3d13911cf69087e774c644644b9a7fbf932c2
Malizia.dev <filipe26f@hotmail.com>
2024-09-27 20:40:26 -0300

    :lipstick:	feat: criação de tela inical

---
+++ app/index.tsx
@@ -0,0 +1,61 @@
+import { Text, View, Image, StyleSheet, Pressable } from "react-native";
+import { colors } from "../constants/colors"
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Image source={require('../assets/images/logo.png')} />
+      <Text style={styles.title}>Dieta<Text style={{ color: colors.white }}>.IA</Text></Text>
+      <Text style={styles.text}>Sua dieta personalizada com inteligência artificial 🍴 </Text>
+      <Pressable style={styles.button}><Text style={styles.buttonText}>Gerar dieta</Text></Pressable>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.background,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingRight: 16,
+    paddingLeft: 16,
+
+  },
+
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: colors.green
+
+  },
+
+  text: {
+    color: 'white',
+    fontSize: 16,
+    width: 240,
+    marginTop: 8,
+    marginBottom: 8,
+    textAlign: 'center',
+
+  },
+
+  button: {
+    backgroundColor: colors.blue,
+    width: '100%',
+    height: 40,
+    borderRadius: 4,
+    marginTop: 34,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: colors.white
+  }
+})
