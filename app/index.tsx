c7a31d62d380603313364cf824b096cdba06bce8
Filipe Malizia <filipe26f@hotmail.com>
2024-10-31 01:17:41 -0300

    feat: Finalização do app dieta ia

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,30 @@
-import { Text, View, Image, StyleSheet, Pressable } from "react-native";
-import { colors } from "../constants/colors"
-import { Link } from "expo-router";
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native'
+import { colors } from '../constants/colors'
+import { Link } from 'expo-router'
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Image source={require('../assets/images/logo.png')} />
-      <Text style={styles.title}>Dieta<Text style={{ color: colors.white }}>.IA</Text></Text>
-      <Text style={styles.text}>Sua dieta personalizada com inteligência artificial 🍴 </Text>
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/images/logo.png')}
+      />
+
+      <Text style={styles.title}>
+        Dieta<Text style={{ color: colors.white }}>.AI</Text>
+      </Text>
+
+      <Text style={styles.text}>
+        Sua dieta personalizada com inteligência artificial
+      </Text>
+
       <Link href="/step" asChild>
-        <Pressable style={styles.button}><Text style={styles.buttonText}>Gerar dieta</Text></Pressable>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Gerar dieta</Text>
+        </Pressable>
       </Link>
+
     </View>
-  );
+  )
 }
 
 
@@ -22,43 +32,36 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.background,
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingRight: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
     paddingLeft: 16,
-
+    paddingRight: 16,
   },
-
   title: {
     fontSize: 36,
     fontWeight: 'bold',
     color: colors.green
-
   },
-
   text: {
-    color: 'white',
     fontSize: 16,
+    color: colors.white,
     width: 240,
+    textAlign: 'center',
     marginTop: 8,
     marginBottom: 8,
-    textAlign: 'center',
-
   },
-
   button: {
     backgroundColor: colors.blue,
     width: '100%',
     height: 40,
     borderRadius: 4,
-    marginTop: 34,
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
+    marginTop: 34,
   },
-
   buttonText: {
-    fontWeight: 'bold',
+    color: colors.white,
     fontSize: 16,
-    color: colors.white
+    fontWeight: 'bold'
   }
 })
--- app/step/index.tsx
+++ app/step/index.tsx
@@ -1,11 +1,51 @@
-import { View, Text, StyleSheet, ScrollView, Pressable } from "react-native";
-import { colors } from "@/constants/colors";
-import { Header } from "@/components/header";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Pressable,
+    ScrollView
+} from 'react-native'
+import { colors } from '../../constants/colors'
+import { Header } from '../../components/header'
+import { Input } from '../../components/input'
+
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { router } from 'expo-router'
+import { useDataStore } from '../../store/data'
 
 
+const schema = z.object({
+    name: z.string().min(1, { message: "O nome é obrigatório" }),
+    weight: z.string().min(1, { message: "O peso é obrigatório" }),
+    age: z.string().min(1, { message: "A idade é obrigatória" }),
+    height: z.string().min(1, { message: "A altura é obrigatória" }),
+})
 
+type FormData = z.infer<typeof schema>
 
 export default function Step() {
+
+    const { control, handleSubmit, formState: { errors, isValid } } = useForm<FormData>({
+        resolver: zodResolver(schema)
+    })
+
+    const setPageOne = useDataStore(state => state.setPageOne)
+
+
+    function handleCreate(data: FormData) {
+        console.log("PASSANDO DADOS DA PAGINA 1");
+
+        setPageOne({
+            name: data.name,
+            weight: data.weight,
+            age: data.age,
+            height: data.height
+        })
+
+        router.push("/create")
+    }
 
     return (
         <View style={styles.container}>
@@ -59,7 +99,6 @@ export default function Step() {
 
         </View>
     )
-
 }
 
 const styles = StyleSheet.create({
